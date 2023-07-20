f0fd2c15be0fb1081902076ed9d77e956c26dad8
karamveer-hub <karamveersachdeva3@gmail.com>
2023-07-20 12:48:38 +0530

    nagaland

---
+++ index.js
@@ -0,0 +1,144 @@
+$('.slider ').slick({
+    infinite: true,
+    speed: 300,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrow: true,
+    dots: true,
+    prevArrow: "<img class=' slick-prev' src='images/next.png' style='transform:rotate(180deg)'>",
+    nextArrow: "<img class=' slick-next' src='images/next.png'>",
+
+});
+
+let burgerIcon = document.querySelector("#burger")
+let mobsearch = document.querySelector(".mob-header-search")
+let mobMenu = document.querySelector(".mob-menu-display")
+let searchMobDisplay = document.querySelector(".mob-menu-display #searchMobDisplay")
+let searchMobDisplayLi = document.querySelectorAll(".dropdownli")
+let searchDeskDisplayLi = document.querySelectorAll(".desktop-menu li")
+
+burgerIcon.addEventListener("click", () => {
+    mobMenu.classList.toggle("show")
+    burgerIcon.classList.toggle("activate")
+})
+mobsearch.addEventListener("click", () => {
+    mobMenu.classList.toggle("show")
+    burgerIcon.classList.toggle("activate")
+    searchMobDisplay.classList.toggle("activate")
+
+    searchMobDisplay.children[1].focus()
+})
+searchMobDisplay.addEventListener("click", () => {
+    searchMobDisplay.classList.toggle("activate")
+    if (!searchMobDisplay.classList.contains("activate"))
+        searchMobDisplay.children[1].blur()
+})
+searchMobDisplayLi.forEach(item => {
+    item.addEventListener("click", (e) => {
+
+        if (e.currentTarget.nextElementSibling) {
+            if (e.currentTarget.nextElementSibling.classList.contains("show")) {
+                e.currentTarget.nextElementSibling.classList.remove("show")
+                e.currentTarget.style.transform = null;
+            }
+            else {
+                for (let i = 0; i < searchMobDisplayLi.length; i++) {
+                    searchMobDisplayLi[i].nextElementSibling?.classList.remove("show")
+                    searchMobDisplayLi[i].style.transform = null;
+                }
+                e.currentTarget.nextElementSibling.classList.add("show")
+                e.currentTarget.style.transform = 'rotate(180deg)'
+            }
+        }
+    })
+    item.addEventListener("blur", () => {
+    })
+})
+
+let contentEdit = document.querySelectorAll(".contentEdit")
+contentEdit.forEach(item => {
+    item.addEventListener("input", (e) => {
+        // Save the edited HTML content into innerText
+        // Add an event listener to save the content when it's being edited
+        e.currentTarget.innerText = e.currentTarget.innerHTML;
+
+    })
+})
+
+//   viewmore functionality
+let viewMoreBtn = document.querySelectorAll('.viewMore')
+let viewMoreHomeBtn = document.querySelectorAll('.viewAllHomeBtn')
+let viewMoreDiv = document.querySelectorAll('.viewMoreDiv')
+let viewMoreHomeDiv = document.querySelector('.inner1 > div')
+
+let nextView = 1;
+if (window.matchMedia("(max-width: 1000px)").matches) {
+    
+        viewMoreDiv.forEach(div => {
+            if(div.children.length==2){
+                div.nextElementSibling.style.display='none'
+            }
+            else{
+
+                for (let i = 2; i < div.children.length; i++) {
+                    div.children[i].style.display = "none"
+                    
+                }
+            }
+        })
+
+    
+}
+
+viewMoreBtn.forEach(btn => {
+    btn.addEventListener("click", () => {
+    //    nextView==btn.previousElementSibling.children.length?btn.innerText="View Less":"View More"
+        if(nextView < btn.previousElementSibling.children.length){
+            if(nextView==btn.previousElementSibling.children.length-1){
+                nextView+=1;
+                console.log(nextView)
+
+                for (let i = 2; i < nextView; i++) {
+                    // console.log(nextView, btn.previousElementSibling.children.length, "i")
+                    if (btn.previousElementSibling) btn.previousElementSibling.children[i].style.display = "block"
+                }
+            }
+            else{
+                nextView+=2;
+                console.log(nextView)
+                for (let i = 2; i < nextView; i++) {
+                    // console.log(nextView, btn.previousElementSibling.children.length, "i")
+                    if (btn.previousElementSibling) btn.previousElementSibling.children[i].style.display = "block"
+                }
+            }
+        }
+        else if(nextView==btn.previousElementSibling.children.length){
+            for (let i = nextView-1; i >= 2; i--) {
+                // console.log(nextView, btn.previousElementSibling.children.length, "i")
+                if (btn.previousElementSibling) btn.previousElementSibling.children[i].style.display = "none"
+                
+            }
+            nextView=2
+            // console
+            }
+    })
+})
+viewMoreHomeBtn.forEach(btn => {
+    btn.addEventListener("click", () => {
+        console.log(nextView)
+        btn.parentElement.parentElement.classList.toggle("activate")
+        btn.parentElement.parentElement.classList.contains("activate") ? btn.textContent = "Compact View" : btn.textContent = "View All"
+    })
+})
+
+
+
+let deskSearch = document.querySelector(".desk-search")
+deskSearch.addEventListener("keyup", (e) => {
+    console.log(e.keyCode)
+
+})
+
+
---
+++ realtimeEditing.js
@@ -0,0 +1,57 @@
+let editBtn=document.querySelector(".editbtn")
+editMode=false;
+
+editBtn.addEventListener("click",()=>{
+    if(editMode==false){ editMode=true;
+        editBtn.innerText="Edit Mode On"
+        document.body.style.backgroundColor="#bbbbbb"
+    }
+    else {editMode=false;
+        editBtn.innerText="Edit"
+        document.body.style.backgroundColor="unset"
+    }
+console.log("askla")
+})
+
+
+let aboutUs=document.querySelectorAll(".aboutPage .contentEdit")
+
+
+
+let gettingDataFromLS=JSON.parse(localStorage.getItem("aboutUsPage"))
+if(gettingDataFromLS!=null){
+    Object.values(gettingDataFromLS).map((entry,index) => {
+       aboutUs[index].innerHTML=entry
+    })
+}
+else{
+    let aboutUsPageData={
+        aboutus:"The SDG Coordination Centre (SDGCC) of the Planning & Transformation Department, Government of Nagaland was established in October  2019 with technical assistance from the United Nations Development Programme (UNDP) with a mandate to act as a centre for SDG knowledge dissemination, monitoring, policy strengthening and as an accelerator of SDG implementation across the State.",
+        vision:"We envisage Nagaland as a well-governed, peaceful, inclusive and prosperous state of India with a sustainable and diverse economy, a skilled and healthy population providing equitable opportunities for all citizens who have a sense of belonging and fulfilment in a safe, clean and green environment where none feels deprived and women are empowered with equal opportunities.",
+        mission:"The State of Nagaland is committed to the 2030 Agenda and has set ambitious targets for the State. The State is equipped with the technical knowledge, capacity, resources and system to implement and achieve the Nagaland SDG Vision 2030."
+    }
+    localStorage.setItem("aboutUsPage",JSON.stringify(aboutUsPageData))
+}
+
+
+    aboutUs.forEach((item,i)=>{
+        item.addEventListener("click",()=>{
+            if(editMode==true) item.contentEditable=true;
+        })
+
+        item.addEventListener("input",()=>{
+            aboutUsPageData={
+                aboutus:aboutUs[0].innerHTML,
+                vision:aboutUs[1].innerText,
+                mission:aboutUs[2].innerText
+            }
+            localStorage.setItem("aboutUsPage",JSON.stringify(aboutUsPageData))
+        })
+        item.addEventListener("blur",()=>{
+            editMode=false;
+            document.body.style.backgroundColor="unset"
+        editBtn.innerText="Edit"
+        })
+    })
+    
+
